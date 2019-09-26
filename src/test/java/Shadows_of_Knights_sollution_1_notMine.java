122aee859b46a200c6fc7e4d54698fe214512a6a
IvanenkoV <ivanenko.vitaliy@gmail.com>
2019-09-26 15:16:32 +0300

    the version of Shedows of Knights 1 (not Mine)

---
+++ src/test/java/Shadows_of_Knights_sollution_1_notMine.java
@@ -0,0 +1,40 @@
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Shadows_of_Knights_sollution_1_notMine {
+
+
+
+
+    @Test
+    public void test5 () {
+        Shadows_of_Knights_1_Scanner in = new Shadows_of_Knights_1_Scanner(System.in);
+
+            int W = in.nextInt(); // width of the building.
+            int H = in.nextInt(); // height of the building.
+            int N = in.nextInt(); // maximum number of turns before game over.
+            int X0 = in.nextInt();
+            int Y0 = in.nextInt();
+
+            // game loop
+            int xa = 0, ya = 0, xz = W-1, yz = H-1;
+            while (true) {
+                String BOMB_DIR = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+                if ( BOMB_DIR.indexOf("R")>=0 ) { xa = X0+1; }
+                if ( BOMB_DIR.indexOf("L")>=0 ) { xz = X0-1; }
+                if ( BOMB_DIR.indexOf("D")>=0 ) { ya = Y0+1; }
+                if ( BOMB_DIR.indexOf("U")>=0 ) { yz = Y0-1; }
+
+                X0 = ((xz-xa)/2) + xa;
+                Y0 = ((yz-ya)/2) + ya;
+
+                System.out.println(X0+" "+Y0); // the location of the next window Batman should jump to.
+            }
+        }
+    }
+
+
